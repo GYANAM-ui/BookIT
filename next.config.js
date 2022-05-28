dbe1a809b59dbedc6879aaea72ffbbc9bb07440b
GYANAM <ggyanam869@gmail.com>
2022-05-28 10:42:17 +0530

    Intial Update

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
         STRIPE_API_KEY: 'pk_test_51L3vL8SJJlCj5EtXVg1fNAYJLEyX0Bu67tvVgqx46nqNhNqvugFjTLurtAhIZlzROWCNNyiAUCwLJ8vcw2ZjnpL200wmSADFBo',
         STRIPE_SECRET_KEY: 'sk_test_51L3vL8SJJlCj5EtXISeekS6MprsPxsn0oMMr8R9ttSq7Cg4rRzT6yiagA4c1Eylrmj82KmEMGGwjW2OhwNg9zWRN00nruMM48o',
 
-        STRIPE_WEBHOOK_SECRET: 'whsec_zDcQgt5UWKeZfkB20DOkTlk9If6ePtf0',
+        STRIPE_WEBHOOK_SECRET: 'whsec_Ut3Bm9pJCdwPkOcFydF2B1DJatAMiWIs',
 
         CLOUDINARY_CLOUD_NAME: 'dbbxqppre',
         CLOUDINARY_API_KEY: '148245814257764',
@@ -16,7 +16,7 @@ module.exports = {
         SMTP_PORT: "2525",
         SMTP_USER: "2dc3b1bea6e6e8",
         SMTP_PASSWORD: "4f674dc07c891c",
-        SMTP_FROM_EMAIL: "noreply@bookit.com",
+        SMTP_FROM_EMAIL: "ggyanam869@gmail.com",
         SMTP_FROM_NAME: "BOOKIT",
 
         NEXTAUTH_URL: 'https://bookitnew.vercel.app',
